430fa5588d9d705534e6796fc499eecf5ab7c457
chenxinlong <237448382@qq.com>
2016-08-10 17:20:22 +0800

    Dev

--- database/182.Duplicate Emails.sql
+++ database/182.Duplicate Emails.sql
@@ -2,3 +2,20 @@
 SELECT Email FROM Person
 GROUP BY Email
 HAVING COUNT(*)>1
+
+git reflog 
+git diff
+git log --pretty=oneline
+git status
+git init 
+git add
+git commit 
+git checkout --"a.txt"
+git rm
+git reset --hard HEAD
+git push
+git pull
+git remote
+git clone
+git remote add NAME git@github.com:cxl/leetcode.git
+ssh-keygen -t rsa -C "email"
