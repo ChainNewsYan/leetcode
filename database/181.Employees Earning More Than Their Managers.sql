8806db774a65139685238a079ea44540f7b25c01
chenxinlong <237448382@qq.com>
2016-11-25 10:46:39 +0800

    modified database

--- database/181.Employees Earning More Than Their Managers.sql
+++ database/181.Employees Earning More Than Their Managers.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 SELECT E1.Name as Employee
 FROM Employee as E1, Employee as E2
 WHERE E1.ManagerId = E2.id and E1.Salary > E2.Salary
--- database/182.Duplicate Emails.sql
+++ database/182.Duplicate Emails.sql
@@ -2,27 +2,3 @@
 SELECT Email FROM Person
 GROUP BY Email
 HAVING COUNT(*)>1
-
-git reflog 
-git diff
-git log --pretty=oneline
-git log -1 --上一次提交信息，同理，git log -2 上两次提交信息
-git status
-git init 
-git add
-git commit 
-git checkout --"a.txt"
-git rm
-git reset --hard HEAD
-git push
-git pull
-git remote
-git clone
-git remote add NAME git@github.com:cxl/leetcode.git
-ssh-keygen -t rsa -C "email"
-git checkout -b Dev
-git checkout master
-git merge Dev
-git branch -d Dev --无法删除自己当前所在的分支，必须先移动到master分支    
-git branch -D Dev --大写D表示强制删除分支，即使这个分支还没有merge到master
-Dev++
