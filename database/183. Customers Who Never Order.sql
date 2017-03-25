afdb676b74b64832446d161c80d032736ace8afd
cinlen_0x05 <237448382@qq.com>
2017-03-26 01:38:51 +0800

    Update 183. Customers Who Never Order.sql

--- database/183. Customers Who Never Order.sql
+++ database/183. Customers Who Never Order.sql
@@ -1,4 +1,9 @@
+--  方法1
 SELECT Name as Customers FROM Customers as c
 WHERE c.Id not in(
     SELECT CustomerId FROM Orders
 )
+
+-- 方法2
+SELECT c.Name as Customers FROM Customers c left join Orders o on c.Id = o.CustomerId 
+WHERE CustomerId is null
